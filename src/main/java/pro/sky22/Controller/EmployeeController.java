102a7cf989f89389138f0484379685c800523956
NikitaShkril <Shkril95@yandex.ru>
2022-04-01 12:15:01 +0300

    Добавил

---
+++ src/main/java/pro/sky22/Controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package pro.sky22.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky22.Employee;
+import pro.sky22.Service.EmployeeService;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public Employee add(@RequestParam("firstName") String firstName,
+                        @RequestParam("lastName") String lastName,
+                        @RequestParam("department") int department,
+                        @RequestParam("salary") int salary) {
+        return employeeService.add(firstName, lastName, department, salary);
+
+    }
+
+    @GetMapping(path = "/remove")
+    public Employee remove(@RequestParam("firstName") String firstName,
+                           @RequestParam("lastName") String lastName) {
+        return employeeService.remove(firstName, lastName);
+    }
+
+    @GetMapping(path = "/find")
+    public Employee find(@RequestParam("firstName") String firstName,
+                         @RequestParam("lastName") String lastName) {
+        return employeeService.find(firstName, lastName);
+    }
+}
---
+++ src/test/java/pro/sky22/ServiceImplTestConstants.java
@@ -0,0 +1,14 @@
+package pro.sky22;
+
+public class ServiceImplTestConstants {
+private final static String EMPTY_NAME = "";
+private final static String DEFAULT_NAME = "stranger";
+private final static String ONLY_SPACE_NAME = "     ";
+private final static String ILLEGAL_CHARACTERS_NAME = "Ivan123";
+private final static String CORRECT_NAME = "Ivan";
+private final static String LOWER_CASE_NAME = "ivan";
+private final static String UPPER_CASE_NAME = "IVAN";
+private final static String DEFAULT_MESSAGE = "Added successful, stranger!";
+private final static String CORRECT_MESSAGE = "Added successful, Ivan!";
+private final static String NOT_FOUND_MESSAGE = "Employee not found!";
+}
